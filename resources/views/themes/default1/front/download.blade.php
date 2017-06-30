d3d6fe62a5c2e598664d6d1064d6e50d1b9ccae1
Vijay Sebastian <vijay.sebastian@ladybirdweb.com>
2017-06-30 16:37:04 +0530

    loader on tables

--- resources/views/themes/default1/front/download.blade.php
+++ resources/views/themes/default1/front/download.blade.php
@@ -18,7 +18,11 @@ Faveo Download
             <div class="page-Download-main">
                 
                 <h2><span >Download</span>&nbsp;<i class="fa fa fa-download "></i></h2>
+               @if(!is_array($release))
                 <p>&nbsp;&nbsp;&nbsp;Your download will begin in a moment. If it doesn't, Click <a href="{{$release}}">here</a> to download.</p>
+               @else 
+               <p>&nbsp;&nbsp;&nbsp;Can not process your request now. Please contact us.</p>
+               @endif
             </div>
         </div>
 
@@ -26,8 +30,10 @@ Faveo Download
 </section>
 @stop
 @section('end')
-<?php 
+<?php
+if(!is_array($release)){
 header("Location: $release"); 
 exit;
+}
 ?>
 @stop
